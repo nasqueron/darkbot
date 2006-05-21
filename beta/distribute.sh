c650be802985c679c5ef3cf77f86985ab8dd2b83
onefang <onefang>
2006-05-21 07:44:57 +0000

    No need to delete the m4 cache here, as it is now done in bootstrap.

--- beta/distribute.sh
+++ beta/distribute.sh
@@ -8,6 +8,5 @@ mkdir $DESTDIR
 cp -p * $DESTDIR 2>/dev/null
 cp -p .* $DESTDIR 2>/dev/null
 cp -a build dat docs m4 scripts source $DESTDIR
-rm -rf $DESTDIR/build/autom4te.cache
 tar cfz build/$DESTDIR.tar.gz $DESTDIR
 rm -rf $DESTDIR
