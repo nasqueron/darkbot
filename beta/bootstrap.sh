eff930f9acb77b19adeb50c3cb37f59b46326d65
onefang <onefang>
2006-05-23 06:38:54 +0000

    alloc and stat variations taken care of.

--- beta/bootstrap.sh
+++ beta/bootstrap.sh
@@ -2,5 +2,5 @@
 
 ./clean.sh
 
-mkdir build && cp source/configure.ac source/Makefile.am build && cp -a dat build && autoreconf -fiv -Wall -Werror build
+mkdir build && cp source/configure.ac source/Makefile.am source/compat/* build && cp -a dat build && autoreconf -fiv -Wall -Werror build
 rm -rf build/autom4te.cache
